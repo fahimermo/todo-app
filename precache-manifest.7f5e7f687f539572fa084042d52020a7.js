627926618ed5d07f6b0f785f4fc1f799c5f331d1
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-16 06:58:16 +0200

    Updates

---
+++ precache-manifest.7f5e7f687f539572fa084042d52020a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43a75bbd31db379633fd3bc1205b330",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "58a07f485b6bea3dce15",
+    "url": "/todo-app/static/css/main.aeff9c4c.chunk.css"
+  },
+  {
+    "revision": "a801eaa858176b6b90de",
+    "url": "/todo-app/static/js/2.b7616527.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-app/static/js/2.b7616527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a07f485b6bea3dce15",
+    "url": "/todo-app/static/js/main.41024c79.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  }
+]);
